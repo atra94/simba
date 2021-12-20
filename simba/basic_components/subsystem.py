fb60caf34a9ec9a71d7dad24be21d6059639715b
Arne <arnetraue@gmx.de>
2021-12-20 19:29:35 +0100

    update

--- simba/basic_components/subsystem.py
+++ simba/basic_components/subsystem.py
@@ -1,16 +1,26 @@
-from simba.core import SystemComponent
+import numpy as np
+from typing import Iterable
+
+import simba.core as core
 
 
-class Subsystem(SystemComponent):
+class Subsystem(core.SystemComponent):
 
     @property
     def components(self):
         return self._components
 
-    def __init__(self, name, components, inputs=(), outputs=()):
+    def __init__(
+            self,
+            name: str,
+            components: Iterable[core.SystemComponent],
+            inputs: Iterable[core.Input] = (),
+            outputs: Iterable[core.Output] = ()
+    ):
         super().__init__(name, inputs, outputs)
-        self._components = components
-        raise NotImplementedError
+        namelist = [component.name for component in components]
+        assert len(set(namelist)) == len(namelist), 'Duplicate names in the components. Use all unique names.'
+        self._components = {component.name: component for component in components}
 
     def compile(self, get_extra_indices, numba_compile=True):
-        raise NotImplementedError
+        pass
